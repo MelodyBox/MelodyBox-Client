6e0014ac27d631f7c1a13ca1df2f2a5c84980257
MrPancakes39 <selenahasan39@proton.me>
2023-04-12 19:47:23 +0300

    Added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
     <div className="App">
-      <p>wow</p>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import styles from "./Header.module.less";
+
+function Header() {
+  return <div id={styles.header}></div>;
+}
+
+export default Header;
