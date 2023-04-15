681bcc2e16a534346376e383073d350923a3d32b
MrPancakes39 <selenahasan39@proton.me>
2023-04-15 20:03:33 +0300

    Added clear results

--- src/components/Main/Results.tsx
+++ src/components/Main/Results.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import type { SearchResult, SongItem, ArtistItem } from "../../API";
 import { useEffectOnce } from "usehooks-ts";
 import styles from "./Main.module.less";
+
+import { ContentContext } from "../ContentState";
 
 function Song({ song }: { song: SongItem }) {
   const [imgSrc, setImgSrc] = useState("");
@@ -40,14 +42,25 @@ type ResultsProps = {
 };
 
 function Results({ results }: ResultsProps) {
-  console.log(results);
+  //   console.log(results);
   //   results = results.slice(0, 1);
+
+  const { setContent, setSearchResult } = useContext(ContentContext);
+
+  function clearResults() {
+    setContent("empty");
+    setSearchResult([]);
+  }
+
   return (
-    <>
+    <div className={styles.results__container}>
+      <button className={styles.clear_results} onClick={clearResults}>
+        Clear Results
+      </button>
       {results.map((item) =>
         "browseId" in item ? <Artist key={item.browseId} artist={item} /> : <Song key={item.videoId} song={item} />
       )}
-    </>
+    </div>
   );
 }
 
