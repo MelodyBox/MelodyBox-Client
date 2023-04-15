384cdc5c356b05e7919f0a01bd4b8d1c8f94e855
MrPancakes39 <selenahasan39@proton.me>
2023-04-15 11:45:06 +0300

    Added Image Attribution in source

--- src/components/Main/EmptyPage.tsx
+++ src/components/Main/EmptyPage.tsx
@@ -4,6 +4,9 @@ import cheery from "./cheerysvg.svg";
 function EmptyPage() {
   return (
     <div className={styles.empty__container}>
+      {/* Image by pikisuperstar on Freepik
+          URL: https://www.freepik.com/free-vector/student-set-with-different-postures_2881095.htm
+       */}
       <img src={cheery} alt="A young woman who is cheery." />
       <span>Start searching for songs now!</span>
     </div>
